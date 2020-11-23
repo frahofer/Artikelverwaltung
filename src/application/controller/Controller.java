530ae3c603fced90538b2987c948190699749c8e
frahofer <frahofer@htl-steyr.ac.at>
2020-11-23 13:54:16 +0100

    Änderung Name

---
+++ src/application/controller/Controller.java
@@ -0,0 +1,30 @@
+package application.controller;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    //userFabian.fxml
+
+    public TextField user_name;
+    public TextField user_title;
+    public TextField user_street;
+    public TextField user_zip;
+    public TextField user_city;
+    public TextField user_country;
+    public ChoiceBox user_department_dropdown;
+
+    //departmentFabian.fxml
+
+    public TextField department_name;
+
+    //commentFabian.fxml
+
+    public TextField comment_title;
+    public TextField comment_date;
+    public TextArea comment_texArea;
+
+
+}
