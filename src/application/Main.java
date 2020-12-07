7ab638f6e0dc70df300acc035edfb2696f5d723d
pgyurdzh <pgyurdzh@htl-steyr.ac.at>
2020-12-07 11:00:25 +0100

    scene fxml priorityPetko.fxml
    Priority Contoller fertig
    
    Scene.fxml geändert
    
    Controller_users neu

--- src/application/Main.java
+++ src/application/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/userFabian.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -8,16 +8,6 @@ import javafx.scene.control.TextField;
 
 public class Controller {
 
-    //userFabian.fxml
-
-    public TextField user_name;
-    public TextField user_title;
-    public TextField user_street;
-    public TextField user_zip;
-    public TextField user_city;
-    public TextField user_country;
-    public ChoiceBox user_department_dropdown;
-
     //departmentFabian.fxml
 
     public TextField department_name;
